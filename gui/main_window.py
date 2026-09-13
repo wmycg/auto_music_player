eb284ecb3ebe86f650fa4c432cd2bdd25ffa51e4
xiaominaimoyu <xiaominaimoyu@users.noreply.github.com>
2026-09-13 13:53:13 +0800

    fix: 更新版本号至 v1.2.6

--- gui/main_window.py
+++ gui/main_window.py
@@ -298,7 +298,7 @@ class MainWindow(QMainWindow):
         self.nav.currentRowChanged.connect(self._switch_page)
         sidebar_layout.addWidget(self.nav, 1)
 
-        footer = QLabel("v1.2.5")
+        footer = QLabel("v1.2.6")
         footer.setObjectName("SidebarFooter")
         sidebar_layout.addWidget(footer)
 
